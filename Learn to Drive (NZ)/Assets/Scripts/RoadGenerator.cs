b6c0d9934b7b53c74c406c9de9e21c83a2429adf
egomm <egomyt@gmail.com>
2023-08-08 14:19:44 +1200

    Working on road generation - need to sort out the angles

--- Learn to Drive (NZ)/Assets/Scripts/RoadGenerator.cs
+++ Learn to Drive (NZ)/Assets/Scripts/RoadGenerator.cs
@@ -51,12 +51,14 @@ public class RoadGenerator : MonoBehaviour {
             Instantiate(straightRoad, new Vector3(0, 0, 13.999f*i), Quaternion.identity);
         }*/
         // Generate between 600 and 800 roads
-        int roundCount = Random.Range(600, 800);
+        int roundCount = Random.Range(6, 8);
         // Measure the current angle in degrees
+        bool changedAngle = false;
         int currentAngle = 0;
         Vector3 previousRoadCoordinates = new Vector3(0, 0, 0);
         Vector3 roadCoordinates = new Vector3(0, 0, 0);
         int previousRoundabout = 0;
+        string previousRoad = "straight";
         for (int i = 0; i < roundCount; i++) {
             // Make sure there can only be a roundabout every 5 roads
             float randomRoadGeneration = Random.Range(0f, 1f);
@@ -66,26 +68,68 @@ public class RoadGenerator : MonoBehaviour {
                     // Roundabout
                     previousRoundabout = i;
                     roadType = twoRoundabout; // Temporary
+                    roadCoordinates = previousRoadCoordinates + new Vector3(0, 0, 13.999f);
+                    previousRoad = "roundabout";
                 } else if (randomRoadGeneration < curveFrequency + roundaboutFrequency) {
                     // Curve: need to make this adjust to the coordinate
                     // If the current angle is 0 degrees, decrease it by 90 to 270 degrees
                     // This needs work 
-                    currentAngle = (currentAngle <= 0) ? 270 : currentAngle - 90;
-                    //Debug.Log(currentAngle);
+                    if (changedAngle) {
+                        currentAngle = (currentAngle <= 90) ? 270 : currentAngle - 180;
+                    } else {
+                        currentAngle = 270;
+                        changedAngle = true;
+                    }
                     roadType = curvedRoad;
+                    // This needs to depend on the curve
+                    //Debug.Log(currentAngle);
+                    if (previousRoad == "straight") {
+                        // This isn't accurate atm
+                        if (currentAngle == 270) {
+                            roadCoordinates = previousRoadCoordinates + new Vector3(-5.7f, 0, 31.1f);
+                        } else if (currentAngle == 90) {
+                            roadCoordinates = previousRoadCoordinates + new Vector3(-24.99f, 0, 0.188f);
+                        }
+                    } else if (previousRoad == "curved") {
+                        roadCoordinates = previousRoadCoordinates + new Vector3(-35.8f, 0, 0.373f);
+                    } else {
+                        // Roundabout (temp)
+                        roadCoordinates = previousRoadCoordinates + new Vector3(-5.7f, 0, 31.1f);
+                    }
+                    previousRoad = "curved";
                 } else {
                     // Straight road
                     roadType = straightRoad;
+                    // Check what the previous road was
+                    if (previousRoad == "straight") {
+                        // change to switch case?
+                        if (currentAngle == 0) {
+                            roadCoordinates = previousRoadCoordinates + new Vector3(0, 0, 13.999f);
+                        } else if (currentAngle == 90) {
+                            roadCoordinates = previousRoadCoordinates + new Vector3(13.999f, 0, 0);
+                        } else if (currentAngle == 270) {
+                            roadCoordinates = previousRoadCoordinates + new Vector3(-13.999f, 0, 0);
+                        }
+                    } else if (previousRoad == "curved") {
+                        // The angle isn't being adjusted correctly
+                        if (currentAngle == 270) {
+                            roadCoordinates = previousRoadCoordinates + new Vector3(-24.99f, 0, 0.182f);
+                        } else {
+                            currentAngle = 180;
+                            roadCoordinates = previousRoadCoordinates + new Vector3(-24.99f, 0, 0.182f);
+                        }
+                    }
+                    previousRoad = "straight";
                 }
             } else {
                 // First road 
                 roadType = straightRoad;
             }
-            if (roadInformation.Count > 0) {
-                // Not starting at the first road
-                roadCoordinates = previousRoadCoordinates + new Vector3(0, 0, 13.999f);
-            }
+            Debug.Log(roadType);
             roadInformation.Add(roadCoordinates, new RoadInformation(roadType, Quaternion.Euler(new Vector3(0, currentAngle, 0))));
+            if (currentAngle == 180) {
+                currentAngle = 90;
+            }
             previousRoadCoordinates = roadCoordinates;
         }
 
